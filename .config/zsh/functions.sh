700ecea3fc29cdab9610ef1ef9932344f8a3ffbf
david rogers <rogersdpdr@gmail.com>
2022-10-15 13:18:54 +0200

    cfgacp

--- .config/zsh/functions.sh
+++ .config/zsh/functions.sh
@@ -9,7 +9,23 @@ acp() {
   done
   msg=""
   vared -p "commit message: " msg
-  git commit -S -m "$msg"
+  git commit -m "$msg"
+  branch=$(git branch --show-current)
+  git push origin $branch
+}
+
+acp() {
+  git status
+  string=""
+  vared -p "files and directories to add: " string
+  array=("${(@s/ /)string}")
+  for el in ${array[*]};
+  do
+    git add $el
+  done
+  msg=""
+  vared -p "commit message: " msg
+  git commit -m -S "$msg"
   branch=$(git branch --show-current)
   git push origin $branch
 }
@@ -27,7 +43,11 @@ cfgacp() {
     $HOME/.config/nvim $HOME/.config/qutebrowser $HOME/.config/scripts \
     $HOME/.config/sounds $HOME/.config/sway $HOME/.config/wallpapers \
     $HOME/.config/libinput-gestures.conf $HOME/.config/waybar $HOME/.config/zathura \
+<<<<<<< HEAD
     $HOME/.config/zsh $HOME/.config/mako
+=======
+    $HOME/.config/zsh
+>>>>>>> 21cf989 (cfgacp)
   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME commit -m 'cfgacp'
   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME push origin master
 }
